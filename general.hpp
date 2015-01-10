628612e3f61c9bdd97da8d6a634d4da77a74a25b
Leo LAPILLONNE <llapillo@e1r5p14.42.fr>
2015-01-10 18:21:24 +0100

    lol

---
+++ general.hpp
@@ -0,0 +1,26 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   general.hpp                                        :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: llapillo <llapillo@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2015/01/10 17:18:13 by llapillo          #+#    #+#             //
+//   Updated: 2015/01/10 17:49:49 by llapillo         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#ifndef GENERAL
+# define GENERAL
+
+#include <ncurses.h>
+#include <iostream>
+#include <stdlib.h>
+
+#define COLUMN 150
+#define ROW 25
+#define QUIT 113
+
+void	init(void);
+
+#endif
