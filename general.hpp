a5b3bc762c70c4e75c660070baad79218778c6fa
Leo Lapillonne <leo.lapillonne@gmail.com>
2015-01-11 02:04:05 +0100

    modif not finish

--- general.hpp
+++ general.hpp
@@ -6,21 +6,28 @@
 //   By: llapillo <llapillo@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/01/10 17:18:13 by llapillo          #+#    #+#             //
-//   Updated: 2015/01/10 17:49:49 by llapillo         ###   ########.fr       //
+//   Updated: 2015/01/11 01:09:42 by llapillo         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
 #ifndef GENERAL
 # define GENERAL
 
-#include <ncurses.h>
+#include <curses.h>
 #include <iostream>
-#include <stdlib.h>
+#include <unistd.h>
 
-#define COLUMN 150
-#define ROW 25
+#define WIDTH 150
+#define HEIGHT 25
 #define QUIT 113
+#define DELAY 500
+
+typedef struct	sShip {
+	float		x;
+	float		y;
+}				t_ship;
 
 void	init(void);
+void	loopGame(void);
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,32 +6,112 @@
 //   By: llapillo <llapillo@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/01/10 17:17:37 by llapillo          #+#    #+#             //
-//   Updated: 2015/01/10 17:52:46 by llapillo         ###   ########.fr       //
+//   Updated: 2015/01/11 01:45:18 by llapillo         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
-#include "general.h"
+#include "general.hpp"
+
+#define MS_PER_FRAME 16
+
+t_ship	ship;
+/*char	map[25][25] =
+{
+	"#########################",
+	"#########################",
+	};*/
+
+int		verifUser() {
+	int		cHeight;
+	int		cWidth;
+	int		input;
+
+	while (cWidth < WIDTH || cHeight < HEIGHT)
+	{
+		input = getch();
+		if (input == QUIT)
+			return(1);
+		getmaxyx(stdscr, cHeight, cWidth);
+		clear();
+		mvprintw(cHeight / 2, (cWidth - 38) / 2, "Not sufficient space for playing game.");
+		usleep(DELAY);
+	}
+	return(0);
+}
+
+void	printMap() {
+
+/*	for (int y = 0; y <= HEIGHT; y++) {
+		for (int x = 0; x <= WIDTH; x++) {
+			if ((y == 0 || y == HEIGHT) && (x == 0 && x == WIDTH))
+				mvprintw(y, x, "#");
+			else
+			  mvprintw(y, x, "#");
+		}
+	}*/
+	for (int x = 0; x < WIDTH; x++)
+		mvprintw(0, x, "#");
+	for (int x = 0; x < WIDTH; x++)
+		mvprintw(HEIGHT, x, "#");
+}
+
+void	inputPlayer() {
+	int		input;
+
+	input = getch();
+	if (input == KEY_UP && ship.y > 1)
+		ship.y -= 1.0f;
+	if (input == KEY_DOWN && ship.y < HEIGHT - 1)
+		ship.y += 1.0f;
+	if (input != KEY_DOWN)
+		std::cout << input;
+}
+
+void	loopGame() {
+	time_t  timev;
+	ship.x = 3.0f;
+	ship.y = 10.0f;
+	while (42) {
+		//double start = time(&timev);
+		/*if (verifUser() == 1)
+		  break ;*/
+		inputPlayer();
+		clear();
+		printMap();
+		mvaddch(ship.y, ship.x, '>');
+		refresh();
+		usleep(DELAY);
+//		sleep(start + (MS_PER_FRAME - time(&timev));
+	}
+}
+
+/*void	initMap() {
+	for (int y = 0; y <= HEIGHT; y++) {
+		for (int x = 0; x <= WIDTH; x++) {
+//			if ((y == 0 || y == HEIGHT) && (x == 0 && x == WIDTH))
+//				mvprintw(y, x, "#");
+			map[y][x] = '#';
+		}
+	}
+	}*/
 
 void	init() {
-	int		height;
-	int		width;
-
 	initscr();
-	raw();
+//	raw();
+	cbreak();
 	noecho();
 	curs_set(0);
+	nodelay(stdscr, TRUE);
 	keypad(stdscr, TRUE);
-	getmaxyx(stdscr, width, height);
-/*	if ()
-	{
-		std::cout << "Not sufficient space for playing game." << std::endl;
-		exit(1);
-		}*/
 	return ;
 }
 
 int		main() {
 	init();
+//	initMap();
+//	std::cout << map;
+//	mvprintw(0, 0, "#");
+	loopGame();
 	endwin();
 	return (0);
 }
